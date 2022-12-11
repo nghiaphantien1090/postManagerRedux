9811b9c59c2f778c30363d22dcd95776003a1cf9
nghiaphantien1090 <nghiaphantien1090@gmail.com>
2022-12-11 15:04:33 +0700

    post list

--- src/features/posts/PostList.tsx
+++ src/features/posts/PostList.tsx
@@ -9,25 +9,29 @@ import store from "../../app/store";
 function PostList({Posts}:{Posts:Post[]}) {
     
     console.log(store.getState())
-    Posts.map(post=>console.log(post.id))
-
-    return (
-        <>
-            <h1 className={clsx(style.my1)}>
-                Post List
-            </h1>
+    const PostExcept =Posts.map(post=>
+    (
             <div className={clsx(style.postExcerpt)}>
                 <h3 >
-                    perferendis adipisci perferendis{1}
+                   {post.title} 
                 </h3>
                 <div className={clsx(style.my1)}>
                 <span>by Greg Marquardt</span> <i>about 2 hours ago</i>
                 </div>
                 <div className={clsx(style.postContent)}>
-                    <p>Ut animi ex facere veritatis dolorum fuga doloremque. Debitis sunt ipsam. Consequuntur expedita amet</p>
+                    <p>{post.content}</p>
                 </div>
                 <ReactionButton/>
             </div>
+    )
+    )
+    console.log(PostExcept)
+    return (
+        <>
+            <h1 className={clsx(style.my1)}>
+                Post List
+            </h1>
+            {PostExcept}
         </>
     )
 }
